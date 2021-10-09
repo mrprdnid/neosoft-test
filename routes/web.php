11082e7fc693b557b94e9017bfa165073c49b480
Imer Prdn <mrprdnid@gmail.com>
2021-10-09 11:36:05 +0700

    add redirect

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/post-list');
 });
 
 Route::get('/post-list', 'PostController@list');
